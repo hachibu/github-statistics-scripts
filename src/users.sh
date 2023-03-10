3d9253360e691ad8136bacc0304132a88d84bca3
Raymond Sohn <raymondsohn@gmail.com>
2023-03-10 13:37:42 -0800

    Update src/users.sh to filter by type

--- src/users.sh
+++ src/users.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 DATA_FILE="data/users.csv"
 
 if [ ! -f "$DATA_FILE" ]; then
-    gh api users --paginate --jq '.[].login' | tee "$DATA_FILE"
+    gh api users --paginate --jq '.[] | select(.type == "User") | .login' | tee "$DATA_FILE"
 fi

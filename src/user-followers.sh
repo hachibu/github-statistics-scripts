68366adfbf4db6f07f42f5cb980790c233d4bc7c
Raymond Sohn <raymondsohn@gmail.com>
2023-03-11 09:44:14 -0800

    Add force flag to scripts

--- src/user-followers.sh
+++ src/user-followers.sh
@@ -4,8 +4,23 @@ set -euo pipefail
 
 source "src/util.sh"
 
+declare -A FLAGS
+
+FLAGS[force]=false
+
+while getopts 'fp' flag; do
+  case "${flag}" in
+    f) FLAGS[force]=true ;;
+    *) ;;
+  esac
+done
+
 DATA_FILE="data/user-followers.csv"
 SAMPLE_SIZE="${1:-100}"
+
+if [ "${FLAGS[force]}" = true ]; then
+    rm -rf "$DATA_FILE"
+fi
 
 if [ ! -f "$DATA_FILE" ]; then
     LOGINS=$(sample_users "$SAMPLE_SIZE")
